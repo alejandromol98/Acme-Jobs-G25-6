6e02103d6b8657d3b5fcfc16f515f8f3f89b4c6e
Santiago <santimartin3105@gmail.com>
2020-01-14 20:18:54 +0100

    Solucionado los problemas al crear el JOB y actualización de las duttis

--- src/main/java/acme/features/authenticated/message/AuthenticatedMessageCreateService.java
+++ src/main/java/acme/features/authenticated/message/AuthenticatedMessageCreateService.java
@@ -22,11 +22,10 @@ import acme.framework.services.AbstractCreateService;
 public class AuthenticatedMessageCreateService implements AbstractCreateService<Authenticated, Message> {
 
 	@Autowired
-	private AuthenticatedMessageRepository	repository;
+	private AuthenticatedMessageRepository repository;
 
 	@Autowired
-	AdministratorCustomisationRepository	customisationRepository;
-
+	AdministratorCustomisationRepository customisationRepository;
 
 	@Override
 	public boolean authorise(final Request<Message> request) {
@@ -34,7 +33,8 @@ public class AuthenticatedMessageCreateService implements AbstractCreateService<
 
 		Person person;
 
-		person = this.repository.findPersons(request.getModel().getInteger("messageThread.id"), request.getPrincipal().getActiveRoleId());
+		person = this.repository.findPersons(request.getModel().getInteger("messageThread.id"),
+				request.getPrincipal().getActiveRoleId());
 
 		return person != null;
 	}
@@ -55,7 +55,8 @@ public class AuthenticatedMessageCreateService implements AbstractCreateService<
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "title", "moment", "tags", "body", "authenticated.userAccount.username", "messageThread.id", "messageThread.title");
+		request.unbind(entity, model, "title", "moment", "tags", "body", "authenticated.userAccount.username",
+				"messageThread.id", "messageThread.title");
 
 		if (request.isMethod(HttpMethod.GET)) {
 			model.setAttribute("accept", "false");
@@ -101,27 +102,39 @@ public class AuthenticatedMessageCreateService implements AbstractCreateService<
 		Boolean spamWordsTags = true;
 		Boolean spamWordsBody = true;
 
-		//		if (entity.getId() != 0) {
+		// if (entity.getId() != 0) {
 		c = this.customisationRepository.findOne();
 		String[] partes = c.getCustomisations().split(",");
 
-		if (m.getTitle() != null && m.getTags() != null) {
+			for (String s : partes) {
+				if (m.getTitle().contains(s)) {
+					spamWordsTitle = false;
+					errors.state(request, spamWordsTitle, "title", "authenticated.message.form.error.title");
 
+				}
+				if (m.getBody().contains(s)) {
+					spamWordsBody = false;
+					errors.state(request, spamWordsBody, "body", "authenticated.message.form.error.body");
+
+				}
+				if (m.getTags().contains(s)) {
+					spamWordsTags = false;
+					errors.state(request, spamWordsTags, "tags", "authenticated.message.form.error.tags");
+
+				}
+			
 		}
-		//		for (String parte : partes) {
-		//			if (entity.getTitle().toLowerCase().contains(parte)) {
-		//				spamWordsTitle = false;
-		//			}
-		//			if (entity.getTags().toLowerCase().contains(parte)) {
-		//				spamWordsTags = false;
-		//			}
-		//			if (entity.getBody().toLowerCase().contains(parte)) {
-		//				spamWordsBody = false;
-		//			}
-		//		}
-		errors.state(request, spamWordsTitle, "title", "authenticated.message.form.error.title");
-		errors.state(request, spamWordsTags, "tags", "authenticated.message.form.error.tags");
-		errors.state(request, spamWordsBody, "body", "authenticated.message.form.error.body");
+		// for (String parte : partes) {
+		// if (entity.getTitle().toLowerCase().contains(parte)) {
+		// spamWordsTitle = false;
+		// }
+		// if (entity.getTags().toLowerCase().contains(parte)) {
+		// spamWordsTags = false;
+		// }
+		// if (entity.getBody().toLowerCase().contains(parte)) {
+		// spamWordsBody = false;
+		// }
+		// }
 	}
 
 	// }
